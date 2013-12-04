999e4b5c743f3bf07b56978dbd877c90907eea56
David Ogutu <ogutu@mit.edu>
2013-12-04 18:54:45 -0500

    stack Overflow stop

--- src/strange_dice/StrangeDiceGUI.java
+++ src/strange_dice/StrangeDiceGUI.java
@@ -5,19 +5,17 @@ import static javax.swing.GroupLayout.Alignment.BASELINE;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Group;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-import javax.swing.GroupLayout.Group;
 import javax.swing.table.DefaultTableModel;
 
 public class StrangeDiceGUI extends JFrame{
@@ -77,34 +75,35 @@ public class StrangeDiceGUI extends JFrame{
 	private final Group fifthVertical;
 
 	public StrangeDiceGUI() {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		rollButton = new JButton();
 		rollButton.setName("rollButton");
-		
+
 		die1array = new ArrayList<Integer>();
 		die2array = new ArrayList<Integer>();
 		die3array = new ArrayList<Integer>();
-		
+
 		die1num1 = new JTextField(20);
 		die1num1.setName("die1num1");
 		die1num2 = new JTextField(20);
 		die1num2.setName("die1num2");
 		die1num3 = new JTextField(20);
 		die1num3.setName("die1num3");
-		
+
 		die2num1 = new JTextField(20);
 		die2num1.setName("die2num1");
 		die2num2 = new JTextField(20);
 		die2num2.setName("die2num2");
 		die2num3 = new JTextField(20);
 		die2num3.setName("die2num3");
-		
+
 		die3num1 = new JTextField(20);
 		die3num1.setName("die3num1");
 		die3num2 = new JTextField(20);
 		die3num2.setName("die3num2");
 		die3num3 = new JTextField(20);
 		die3num3.setName("die3num3");
-		
+
 		die1Text = new JLabel();
 		die1Text.setName("die1Text");
 		die2Text = new JLabel();
@@ -189,6 +188,13 @@ public class StrangeDiceGUI extends JFrame{
 		thirdVertical.addComponent(die3num3);
 
 		fourthVertical = layout.createParallelGroup(BASELINE);
+		//Listen for action from the roll Button
+		rollButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				newRollAction();
+			};
+		});
 		fourthVertical.addComponent(rollButton);
 
 		fifthVertical = layout.createParallelGroup(BASELINE);
@@ -206,33 +212,49 @@ public class StrangeDiceGUI extends JFrame{
 		String [] row0Data = new String[3];
 		row0Data[0] = "Result";
 		tableModel.addRow(row0Data);
-		
+
 		String [] row1Data = {""};
 		tableModel.addRow(row1Data);
 
 		this.pack();
 
-		//Listen for action from the roll Button
-		rollButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				newRollAction();   
-			};
-		});
 	}
 
+	@SuppressWarnings("serial")
 	private void newRollAction(){
 		die1num1String = die1num1.getText();
 		die1num2String = die1num2.getText();
 		die1num3String = die1num3.getText();
-		
+
 		die2num1String = die2num1.getText();
 		die2num2String = die2num2.getText();
 		die2num3String = die2num3.getText();
-		
+
 		die3num1String = die3num1.getText();
 		die3num2String = die3num2.getText();
 		die3num3String = die3num3.getText();
+
+		String[] arr=new String[]{
+				die1num1String,
+				die1num2String,
+				die1num3String,
+
+				die2num1String,
+				die2num2String,
+				die2num3String,
+
+				die3num1String,
+				die3num2String,
+				die3num3String,	};
+
+		for (String str : arr) {
+			if (str==null||str.isEmpty()) {
+				//bad info
+				tableModel.setValueAt("Invalid die entry", 1, 0);
+				this.pack();
+				return;
+			}
+		}
 
 		die1num1Int = Integer.parseInt(die1num1String);
 		die1array.add(die1num1Int);
@@ -240,22 +262,27 @@ public class StrangeDiceGUI extends JFrame{
 		die1array.add(die1num2Int);
 		die1num3Int = Integer.parseInt(die1num3String);
 		die1array.add(die1num3Int);
-		
+
 		die2num1Int = Integer.parseInt(die2num1String);
 		die2array.add(die2num1Int);
 		die2num2Int = Integer.parseInt(die2num2String);
 		die2array.add(die2num2Int);
 		die2num3Int = Integer.parseInt(die2num3String);
 		die2array.add(die2num3Int);
-		
+
 		die3num1Int = Integer.parseInt(die3num1String);
 		die3array.add(die3num1Int);
 		die3num2Int = Integer.parseInt(die3num2String);
 		die3array.add(die3num2Int);
 		die3num3Int = Integer.parseInt(die3num3String);
 		die3array.add(die3num3Int);
-		
+
 		String result = "";
+
+		ArrayList<ArrayList<Integer>> listOfLists = new ArrayList<ArrayList<Integer>>();
+		listOfLists.add(die1array);
+		listOfLists.add(die2array);
+		listOfLists.add(die3array);
 
 		List<List<Integer>> products = Solver.product(die1array, die2array, die3array);
 		int aPos = 0, bPos = 1, cPos = 2;
@@ -285,6 +312,7 @@ public class StrangeDiceGUI extends JFrame{
 
 	public static void main(final String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				StrangeDiceGUI main = new StrangeDiceGUI();
 
